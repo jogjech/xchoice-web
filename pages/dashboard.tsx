2530b07adf5d8b7a5f10411370af6eee07d02f2f
Kangxu Wang <kangxu@umich.edu>
2020-11-30 20:31:02 -0800

    Undo the fix of importing dashboard.module.css as dashboard.tsx is a page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { Col, Row, Button, Alert } from "antd";
 import { PlusSquareOutlined } from "@ant-design/icons";
 import { SurveyMetadata } from "../models/survey";
-import styles from "./Dashboard.module.css";
+import styles from "./dashboard.module.css";
 
 interface Props {}
 
